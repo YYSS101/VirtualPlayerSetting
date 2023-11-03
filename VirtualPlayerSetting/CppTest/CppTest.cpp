195f21795da524529e2cf7bd98b5c95f8bde69f2
m.ueda <m-ueda@denkosha.co.jp>
2023-11-03 16:36:44 +0900

    考え中

--- VirtualPlayerSetting/CppTest/CppTest.cpp
+++ VirtualPlayerSetting/CppTest/CppTest.cpp
@@ -4,22 +4,10 @@
 #include <iostream>
 
 
-typedef int ( *FUNC1 )( int a, int b );
 
 int main()
 {
-  auto hModule = LoadLibrary( L"SampleDLL.dll" );
-//  auto hModule = LoadLibrary( L"C:\\WorkSpace\\VirtualPlayerSetting\\SampleDLL\\bin\Debug\\net6.0\\SampleDLL.dll" );
 
-
-  if( NULL == hModule ) return 1;
-
-
-  FUNC1 sum = (FUNC1)GetProcAddress( hModule, "Sum" );
-
-  int val = sum( 1, 2 );
-
-  std::cout << val << std::endl;
 
   return 0;
 }
